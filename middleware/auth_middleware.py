5c41877b74255b6a369a5e20d97170054de10041
Muhammed-Unais <muhammmedunaisq@gmail.com>
2024-12-24 19:15:58 +0530

    Implement JWT-based user data retrieval and refactor authentication
    1.Integrate JWT token authentication into the login route
    2.Create a new endpoint for retrieving the current user's data, secured by JWT verification
    3. Develop an authentication middleware to validate tokens and user sessions, applied as a dependency across all FastAPI routes

---
+++ middleware/auth_middleware.py
@@ -0,0 +1,19 @@
+from fastapi import HTTPException
+from fastapi.params import Header
+import jwt
+
+
+def auth_middleware(x_auth_token = Header()):
+    try:
+        if not x_auth_token:
+            raise HTTPException(401,"No auth token , verification failed")
+        
+        verfied_token = jwt.decode(x_auth_token,'password_key',['HS256'])
+        if not verfied_token:
+            raise HTTPException(401,'Token verification failed, autherization failed')
+    
+        uid = verfied_token.get('id')
+        return {"uid":uid,"token":x_auth_token}
+    
+    except jwt.PyJWTError:
+        raise HTTPException(401,"Token is not valid, authorization failed")
